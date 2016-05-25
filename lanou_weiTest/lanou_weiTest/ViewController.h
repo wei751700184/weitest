7dc2b87a3d100911e9b7bb4529a3376eb1a44976
lanou <751700184@qq.com>
2016-05-25 15:50:56 +0800

    新建了一个项目

---
+++ lanou_weiTest/lanou_weiTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lanou_weiTest
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 王健伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ lanou_weiTest/lanou_weiTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  lanou_weiTest
+//
+//  Created by lanou on 16/5/25.
+//  Copyright © 2016年 王健伟. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
